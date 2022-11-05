0549a7c6760c25f691de8847194f8fb401fbd99b
Hanna Akalatovich <zvezda0864@gmail.com>
2022-11-05 15:33:39 +0100

    Upload heroIcons package

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 function Header() {
   return (
     <div className="bg-[#F0F5F5] w-full py-6 items-center justify-between flex px-12">
       <div className="w-full lg:flex hidden space-x-4 items-center justify-start py-2">
+        <MagnifyingGlassIcon className="h-4 w-4" />
         <input
           type="text"
           placeholder="Search here"
